ea76a10c12be873d3fa8be3855583782d8df9ff3
fgdsgfdg <156791585+fgdsgfdg@users.noreply.github.com>
2024-02-27 09:15:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exam_Task
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input X: ");
+            int X = int.Parse(Console.ReadLine());
+            double Y = Math.Log(X);
+            Console.WriteLine($"Y = {Y}");
+            Console.ReadKey();
+        }
+    }
+}
